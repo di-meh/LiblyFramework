4205849feb5eae98c4291d27a9e6c869c8b70b9b
Mides <m1d3s@protonmail.com>
2021-02-23 22:53:23 +0100

    Début des pages + tables & sidebar in progress

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,5 @@ let mix = require('laravel-mix');
 mix.disableNotifications();
 mix.setPublicPath('dist');
 mix.js('src/js/main.js', 'dist');
+// mix.babel(['src/js/jquery-3.5.1.min.js', 'src/js/main.js'], "dist/main.js");
 mix.sass('src/sass/main.scss', 'dist');
