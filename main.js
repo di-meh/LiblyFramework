50cace43dbb79c1b4887773c44a71515ee0cde3c
Mides <m1d3s@protonmail.com>
2021-06-17 15:00:17 +0200

    Beginning of Libly CSS FRAMEWORK rework
    First commit of rework
    
    gitignore works ?
    
    Basic Scaffolding of framework

---
+++ main.js
@@ -0,0 +1,9 @@
+import './src/scss/index.scss'
+
+import './node_modules/jquery/dist/jquery'
+import './src/js/index'
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite! </h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
