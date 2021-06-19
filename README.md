05a24f925f09344e5e00184bf25b502d1d765959
Mides <m1d3s@protonmail.com>
2021-06-19 17:43:28 +0200

    Version 2.0 du framework CSS
    README + fixes
    
    beginning of forms
    
    More stuff bref je m'en fous
    
    Almost done + must check warnings
    
    Normalement version finale

---
+++ README.md
@@ -0,0 +1,31 @@
+# Libly : A SCSS Framework for a school project
+
+## Installation
+
+`npm install` or `yarn` (yarn is recommended)
+
+## Available commands
+
+### dev
+
+`npm run dev` or `yarn dev`
+
+Starts a dev server to preview the website
+
+### build
+
+`npm run build` or `yarn build`
+
+Builds optimized html, css & js files in the `dist/` directory
+
+### watch
+
+`npm run watch` or `yarn watch`
+
+Watches any changes in files & compiles every time.
+
+### serve
+
+`npm run serve` or `yarn serve`
+
+Start a production server to preview the bundled files.
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,28 @@
-const { resolve } = require('path')
-
-module.exports = {
+// import autoprefixer from "autoprefixer"
+import { resolve } from "path"
+import { defineConfig } from 'vite'
+import autoprefixer from "autoprefixer"
+export default defineConfig({
     build: {
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                dashboard: resolve(__dirname, 'dashboard/index.html')
+                dashboard: resolve(__dirname, 'dashboard/index.html'),
+                login: resolve(__dirname, 'login/index.html'),
+                register: resolve(__dirname, 'register/index.html'),
+                books: resolve(__dirname, 'books/index.html')
             },
         }
     },
     assetsInclude: ['woff2?',
         'eot',
         'ttf',
-        'otf',]
-}
+        'otf',],
+    css: {
+        postcss: {
+            plugins: [
+                autoprefixer
+            ]
+        }
+    }
+})
