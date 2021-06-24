2f0b6b2ccf52637e7e38f38210f30e7ac8b37058
Mides <m1d3s@protonmail.com>
2021-06-25 01:49:56 +0200

    fixed layout width issues

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 // import autoprefixer from "autoprefixer"
-import path, { resolve } from "path"
+import path from "path"
 import { defineConfig } from 'vite'
 import autoprefixer from "autoprefixer"
 export default defineConfig({
@@ -10,13 +10,6 @@ export default defineConfig({
             formats: ['es']
         },
         rollupOptions: {
-            // input: {
-            //     main: resolve(__dirname, 'index.html'),
-            //     dashboard: resolve(__dirname, 'dashboard/index.html'),
-            //     login: resolve(__dirname, 'login/index.html'),
-            //     register: resolve(__dirname, 'register/index.html'),
-            //     books: resolve(__dirname, 'books/index.html')
-            // },
             output: {
                 assetFileNames: '[name][extname]',
                 chunkFileNames: '[name].js',
