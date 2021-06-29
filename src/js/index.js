566f5d01bfdf779b2d0d344233cdee01a52e6011
Mides <m1d3s@protonmail.com>
2021-06-29 16:14:04 +0200

    Added closable alerts

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,12 @@
 import Chart from 'chart.js/auto';
 import jQuery from 'jquery';
 window.Chart = Chart;
-window.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+$(document).ready(function (){
+    //Alert
+    $('.alert-close').click(function () {
+        $(this).parent('.alert').addClass('disposed');
+    });
+});
+
