48445997d1a5be2fb6c9e6f27f9a1f92db44a3aa
Mehdi Saber <m1d3s@protonmail.com>
2021-07-16 15:35:45 +0200

    Installed DataTables

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,15 @@
 import Chart from 'chart.js/auto';
 import jQuery from 'jquery';
+const dt = require('datatables.net')(window, jQuery);
 window.Chart = Chart;
 window.$ = window.jQuery = jQuery;
-
+window.dt = dt;
 $(document).ready(function (){
     //Alert
     $('.alert-close').click(function () {
         $(this).parent('.alert').addClass('disposed');
     });
+    //Tables (with DataTables)
+    $('.table').DataTable();
 });
 
