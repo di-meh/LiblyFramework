7c0a20416f4819567df96028fd3ef8dce1e72c4d
Mehdi Saber <m1d3s@protonmail.com>
2021-07-18 13:08:33 +0200

    Tentative de fix de DataTable

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,13 @@
 import Chart from 'chart.js/auto';
 import jQuery from 'jquery';
-const dt = require('datatables.net')(window, jQuery);
+//const dt = require('datatables.net')(window, jQuery);
+import 'datatables.net';
+import 'datatables.net-dt/css/jquery.dataTables.min.css';
+import dt from 'datatables.net-dt/js/dataTables.dataTables.min';
 window.Chart = Chart;
 window.$ = window.jQuery = jQuery;
-window.dt = dt;
+window.$.DataTable = dt;
+//window.dt = dt;
 $(document).ready(function (){
     //Alert
     $('.alert-close').click(function () {
