b4bf8610b00448626df03f967ee431b8455b5c65
Mehdi Saber <m1d3s@protonmail.com>
2021-07-23 17:53:31 +0200

    Installed Select2

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,14 @@ import Chart from 'chart.js/auto';
 import jQuery from 'jquery';
 import dt from 'datatables.net';
 import 'datatables.net-dt/css/jquery.dataTables.min.css';
+import 'select2';
+import 'select2/dist/css/select2.css';
 
 window.Chart = Chart;
 window.$ = window.jQuery = jQuery;
 dt(window, $);
 window.$.DataTable = dt;
+window.select2 = select2;
 
 $(document).ready(function (){
     //Alert
@@ -19,5 +22,7 @@ $(document).ready(function (){
             url: "https://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json"
         }
     });
+    //Select2 pour les select multiple
+    $('.select').select2();
 });
 
